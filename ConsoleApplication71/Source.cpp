c01116ab185d200e8846a6b706fd383fe2d6d9e7
isaev <isaev@DESKTOP-VRU7T19>
2017-02-06 21:44:12 +0400

    1
    1

--- ConsoleApplication71/Source.cpp
+++ ConsoleApplication71/Source.cpp
@@ -2,11 +2,13 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 struct lol {
 	string fname;
 	string lname;
+	pair <string, string> name;
 	double srz;
 };
 bool sor(lol a, lol b) {
@@ -23,6 +25,9 @@ int main() {
 		lol zn;
 		zn.fname = fname; 
 		zn.lname = lname;
+		lol zn; 
+		zn.name.first = fname;
+		zn.name.second = lname;
 		l = (a + b + c) / 3;
 		zn.srz = l;
 		allzn[i] = zn;
@@ -30,6 +35,7 @@ int main() {
 	stable_sort(allzn.begin(), allzn.end(), sor);
 	for (auto now : allzn) {
 		cout << now.fname << " " << now.lname << endl;
+		cout << now.name.first << " " << now.name.second << endl;
 	}
 	return 0;
 }
